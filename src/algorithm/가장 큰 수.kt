de26e08235521a5efd7fdfe9fbb651cccaa8a0f6
ahribori <ahribori@gmail.com>
2019-07-30 23:57:52 +0900

    bug fix

--- src/algorithm/가장 큰 수.kt
+++ src/algorithm/가장 큰 수.kt
@@ -2,10 +2,11 @@ package algorithm
 
 class Solution5 {
     fun solution(numbers: IntArray): String {
-        return numbers
+        val answer = numbers
             .map { it.toString() }
             .sortedWith(Comparator { a, b -> (b + a).toInt() - (a + b).toInt() })
             .joinToString ("") { it }
+        return if (answer.startsWith('0')) '0'.toString() else answer
     }
 }
 
